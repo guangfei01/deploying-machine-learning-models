92854b46a11306aa5e528c29ec6dc0b3b57b56b2
jjg werty <jjgwerty@jjgs-MacBook-Air.local>
2026-05-01 10:28:33 -0500

    add requirments update

--- section-05-production-model-package/tests/test_features.py
+++ section-05-production-model-package/tests/test_features.py
@@ -5,8 +5,8 @@ from regression_model.processing.features import TemporalVariableTransformer
 def test_temporal_variable_transformer(sample_input_data):
     # Given
     transformer = TemporalVariableTransformer(
-        variables=config.model_config.temporal_vars,  # YearRemodAdd
-        reference_variable=config.model_config.ref_var,
+        variables=config.model_settings.temporal_vars,  # YearRemodAdd
+        reference_variable=config.model_settings.ref_var,
     )
     assert sample_input_data["YearRemodAdd"].iat[0] == 1961
 
