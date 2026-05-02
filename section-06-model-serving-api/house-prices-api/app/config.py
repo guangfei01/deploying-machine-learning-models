a316d1f1b106b5af23c0d32dda531e6498542c15
jjg werty <jjgwerty@jjgs-MacBook-Air.local>
2026-05-02 10:44:50 -0500

    Modify for Railway deployment

--- section-06-model-serving-api/house-prices-api/app/config.py
+++ section-06-model-serving-api/house-prices-api/app/config.py
@@ -4,7 +4,8 @@ from types import FrameType
 from typing import List, cast
 
 from loguru import logger
-from pydantic import AnyHttpUrl, BaseSettings
+from pydantic import AnyHttpUrl
+from pydantic_settings import BaseSettings
 
 
 class LoggingSettings(BaseSettings):
@@ -28,8 +29,7 @@ class Settings(BaseSettings):
 
     PROJECT_NAME: str = "House Price Prediction API"
 
-    class Config:
-        case_sensitive = True
+    model_config = {"case_sensitive": True}
 
 
 # See: https://loguru.readthedocs.io/en/stable/overview.html#entirely-compatible-with-standard-logging  # noqa
